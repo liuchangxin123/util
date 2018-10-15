7458915cb0b41554201829925526dc4d538ffc44
刘长鑫 <liuchangxin@mimidai.com>
2018-10-15 14:04:56 +0800

    提交util

---
+++ spring-util/src/main/java/com/lcx/springutil/SpringUtilApplication.java
@@ -0,0 +1,12 @@
+package com.lcx.springutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUtilApplication.class, args);
+	}
+}
